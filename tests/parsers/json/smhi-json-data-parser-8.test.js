5a8d3fc54a1eccc49e197132e920ade6d628f7c9
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2019-11-30 23:15:04 +0100

    feat: added support for snowdepth json

---
+++ tests/parsers/json/smhi-json-data-parser-8.test.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const parser = require('../../../src/parsers/json/smhi-json-parser');
+
+const jsonData = fs.readFileSync(
+  './tests/parsers/json/testdata/stockholm-latest-months-8.json',
+  'utf8'
+);
+
+describe('SMHI json data parser parameter 8', () => {
+  it('should parse file into objects', async () => {
+    const parsed = await parser.parse(jsonData, {
+      dateHeader: { text: 'date' },
+      valueHeader: { text: 'value' },
+      valueAttributeName: 'snowDepth'
+    });
+
+    expect(parsed.length).toEqual(2);
+    expect(parsed[0].date).toEqual('2019-10-07');
+    expect(parsed[0].snowDepth).toEqual('0.00');
+    expect(parsed[1].date).toEqual('2019-11-26');
+    expect(parsed[1].snowDepth).toEqual('0.21');
+  });
+});
