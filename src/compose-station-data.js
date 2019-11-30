08766f6cc17a7aec5eba2f5726942361f4192668
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2019-11-30 18:21:37 +0100

    feat: compose items from separate attribute sources

---
+++ src/compose-station-data.js
@@ -0,0 +1,23 @@
+function compose(sources) {
+  const result = [];
+
+  sources.forEach(source => {
+    const dateItems = result.filter(dateItem => dateItem.date === source.date);
+    const isAdded = dateItems.length;
+
+    const dateItem = isAdded ? dateItems[0] : { date: source.date };
+
+    const attributes = Object.keys(source).filter(key => key !== 'date');
+    attributes.forEach(attribute => {
+      dateItem[attribute] = source[attribute];
+    });
+
+    if (!isAdded) result.push(dateItem);
+  });
+
+  return result;
+}
+
+module.exports = {
+  compose
+};
--- tests/compose-station-data.test.js
+++ tests/compose-station-data.test.js
@@ -2,6 +2,63 @@ const composer = require('../src/compose-station-data');
 
 describe('Weather station composer validator', () => {
   it('should compose items from separate attribute sources', async () => {
-    // expect(validator.isDate('2019-12-12')).toBe(true);
+    const sources = [
+      {
+        date: '2019-12-01',
+        temperatureAverage: '2.1'
+      },
+      {
+        date: '2019-12-01',
+        windSpeedAverage: '4.6'
+      },
+      {
+        date: '2019-12-01',
+        rainAndSnowAmount: '0.1'
+      },
+      {
+        date: '2019-12-01',
+        snowDepth: '0.0'
+      },
+      {
+        date: '2019-12-01',
+        temperatureMin: '0.7'
+      },
+      {
+        date: '2019-12-01',
+        temperatureMax: '3.2'
+      },
+      {
+        date: '2019-12-02',
+        temperatureAverage: '0.1'
+      },
+      {
+        date: '2019-12-02',
+        temperatureMin: '-2.6'
+      },
+      {
+        date: '2019-12-02',
+        temperatureMax: '0.5'
+      }
+    ];
+
+    const result = composer.compose(sources);
+
+    expect(result.length).toBe(2);
+
+    expect(result[0].date).toBe('2019-12-01');
+    expect(result[0].temperatureAverage).toBe('2.1');
+    expect(result[0].windSpeedAverage).toBe('4.6');
+    expect(result[0].rainAndSnowAmount).toBe('0.1');
+    expect(result[0].snowDepth).toBe('0.0');
+    expect(result[0].temperatureMin).toBe('0.7');
+    expect(result[0].temperatureMax).toBe('3.2');
+
+    expect(result[1].date).toBe('2019-12-02');
+    expect(result[1].temperatureAverage).toBe('0.1');
+    expect(result[1].windSpeedAverage).toBeUndefined();
+    expect(result[1].rainAndSnowAmount).toBeUndefined();
+    expect(result[1].snowDepth).toBeUndefined();
+    expect(result[1].temperatureMin).toBe('-2.6');
+    expect(result[1].temperatureMax).toBe('0.5');
   });
 });
