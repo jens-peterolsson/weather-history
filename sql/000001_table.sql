550ebed3a7e6828671823cedc3cfc8e7b8d328b3
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2020-02-19 09:45:50 +0100

    feat: sql for creating table

---
+++ sql/000001_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS weatherdates (
+    weatherDate VARCHAR(10) PRIMARY KEY,
+    temperatureAverage DECIMAL(8, 2),
+    windSpeedAverage DECIMAL(8, 2),
+    rainAndSnowAmount DECIMAL(8, 2),
+    snowDepth DECIMAL(8, 2),
+    temperatureMin DECIMAL(8, 2),
+    temperatureMax DECIMAL(8, 2),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modifiedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
